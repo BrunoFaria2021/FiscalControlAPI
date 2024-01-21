1d3507c7b291b929483284d12d26477b982898c3
BrunoFaria2021 <bmc_ti@outlook.com>
2024-01-21 10:59:55 -0300

    Criação de interfaces e repositoris, projeto aparti de agora sera construido por feature

---
+++ FiscalControl/FiscalControl.Infra/Data/Interfaces/ICategoriaRepository.cs
@@ -0,0 +1,14 @@
+﻿using FiscalControl.CrossCutting.Extensions;
+using FiscalControl.Domain.Entities;
+
+namespace FiscalControl.Infra.Data.Interfaces
+{
+    public interface ICategoriaRepository
+    {
+        RetornoApi<List<Categoria>> BuscarTodasCategorias();
+        RetornoApi<Categoria> BuscarCategoria(Guid id);
+        RetornoApi<Categoria> CriarCategoria(Categoria categoria);
+        RetornoApi<Categoria> EditarCategoria(Guid id, Categoria categoria);
+        RetornoApi<bool> DeletarCategoria(Guid id);
+    }
+}
---
+++ FiscalControl/FiscalControl.Infra/Data/Interfaces/ITransacaoRepository.cs
@@ -0,0 +1,14 @@
+﻿using FiscalControl.CrossCutting.Extensions;
+using FiscalControl.Domain.Entities;
+
+namespace FiscalControl.Infra.Data.Interfaces
+{
+    public interface ITransacaoRepository
+    {
+        RetornoApi<List<Transacao>> BuscarTodasTransacoesPorUsuario(Guid usuarioId);
+        RetornoApi<Transacao> BuscarTransacao(Guid id, Guid usuarioId);
+        RetornoApi<Transacao> CriarTransacao(Transacao transacao);
+        RetornoApi<Transacao> EditarTransacao(Guid id, Guid usuarioId, Transacao transacao);
+        RetornoApi<bool> DeletarTransacao(Guid id, Guid usuarioId);
+    }
+}
